e295356670c6da9c9a6ea9532761596f2fcc81a2
TamiWien <tamivnet@gmail.com>
2024-08-26 16:30:45 +0300

    update all products

--- src/pages/Packages.jsx
+++ src/pages/Packages.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getProducts } from '../services/productsService';
 
 const Packages = () => {
+  const [productList, setproductList] = useState([]);
+
+  const fetchData = async () => {
+    try {
+        const product = await getProducts();
+        setproductList(product);
+        console.log(product);
+    } catch (error) {
+        console.error('Error fetching products:', error);
+    }
+};
+
+useEffect(() => {
+    fetchData();
+}, []);
+
   return (
     <div id='packages'>
         <div id='packagesTitleBox' className='titleBox'>
@@ -9,7 +26,23 @@ const Packages = () => {
           </div>
         </div>
         <div id='packagesContent'>
-            
+        <div id='productsBox'>
+            {productList.filter(p => p.categoryId === 1).map((p) => (
+              <>
+                  <div className='productBox' key={p.productId}>
+                    <div className='productBoxIn'>
+                      <img className='productImg' src={p.imagePath} alt={p.productName} />
+                      <div className='productName' >{p.productName}</div>
+                      <div className='productPrice' > ₪{p.price}</div>
+                      <div className='productBtns'>
+                        <input className='productQuantity' type="number" min="1" max="99999" step="1" value="1"/>
+                        <button className='addToCartBtn' >הוסף לסל</button>
+                      </div>
+                    </div>
+                  </div>
+              </>
+            ))}
+          </div>
         </div>
     </div>
   )
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getProducts } from '../services/productsService';
 
 const Products = () => {
+  const [productList, setproductList] = useState([]);
+
+  const fetchData = async () => {
+    try {
+        const product = await getProducts();
+        setproductList(product);
+        console.log(product);
+    } catch (error) {
+        console.error('Error fetching products:', error);
+    }
+};
+
+useEffect(() => {
+    fetchData();
+}, []);
+
   return (
     <div id='products'>
         <div id='productsTitleBox' className='titleBox'>
@@ -9,7 +26,23 @@ const Products = () => {
           </div>
         </div>
         <div id='productsContent'>
-            
+          <div id='productsBox'>
+            {productList.filter(p => p.categoryId === 0).map((p) => (
+              <>
+                  <div className='productBox' key={p.productId}>
+                    <div className='productBoxIn'>
+                      <img className='productImg' src={p.imagePath} alt={p.productName} />
+                      <div className='productName' >{p.productName}</div>
+                      <div className='productPrice' > ₪{p.price}</div>
+                      <div className='productBtns'>
+                        <input className='productQuantity' type="number" min="1" max="99999" step="1" value="1"/>
+                        <button className='addToCartBtn' >הוסף לסל</button>
+                      </div>
+                    </div>
+                  </div>
+              </>
+            ))}
+          </div>
         </div>
     </div>
   )
